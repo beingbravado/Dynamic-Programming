4116fffa0433493e4e09b6ae88c53e88c4ec6b09
beingbravado <sharmagyanesh199@gmail.com>
2021-05-24 13:18:43 +0530

    Maximal Square

--- MaximalSquare.java
+++ MaximalSquare.java
@@ -2,19 +2,38 @@ import java.util.Scanner;
 
 public class MaximalSquare {
     public static int maximalSquare(char[][] matrix) {
-
-    }
-    public static void main(String[] args) throws Exception {
-        Scanner scan = new Scanner(System.in);
-        int n = scan.nextInt();
-        int m = scan.nextInt();
-        int[][] a = new int[n][m];
-        for (int i = 0; i < n; i++) {
-            for (int j = 0; j < m; j++) {
-                a[i][j] = scan.nextInt();
+        int m = matrix.length, n = matrix[0].length;
+        // System.out.println(m + " " + n);
+        int[][] dp = new int[m + 1][n + 1];
+        int ans = 0;
+        for (int i = 0; i <= m; i++) {
+            for (int j = 0; j <= n; j++) {
+                if (i == 0 || j == 0) {
+                    dp[i][j] = 0;
+                    continue;
+                }
+                if (matrix[i - 1][j - 1] == '0') {
+                    dp[i][j] = 0;
+                } else {
+                    dp[i][j] = 1 + Math.min(dp[i - 1][j - 1], Math.min(dp[i - 1][j], dp[i][j - 1]));
+                }
+                ans = Math.max(ans, dp[i][j]);
             }
         }
-        System.out.println(maximalSquare(a));
+        return ans * ans;
+    }
+
+    public static void main(String[] args) throws Exception {
+        Scanner scan = new Scanner(System.in);
+        int m = scan.nextInt();
+        int n = scan.nextInt();
+        char[][] matrix = new char[m][n];
+        for (int i = 0; i < m; i++) {
+            char[] s = scan.next().trim().toCharArray();
+            for (int j = 0; j < n; j++)
+                matrix[i][j] = s[j];
+        }
+        System.out.println(maximalSquare(matrix));
         scan.close();
     }
 }
