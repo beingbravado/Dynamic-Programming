52318a864fb77791aeadc0d6571ce136e1fc330f
beingbravado <sharmagyanesh199@gmail.com>
2021-05-24 12:26:42 +0530

    minimum path sum

--- MinimumPathSum.java
+++ MinimumPathSum.java
@@ -4,26 +4,26 @@ public class MinimumPathSum {
     public static int minPathSum(int[][] grid) {
         int m = grid.length;
         int n = grid[0].length;
-        boolean[][] visited = new boolean[m][n];
-        int ans = Integer.MAX_VALUE;
-        path(grid, visited, m, n, 0, 0, 0, ans);
-        return ans;
-    }
+        int[][] dp = new int[m + 1][n + 1];
+        for (int i = 0; i <= m; i++) {
+            for (int j = 0; j <= n; j++) {
+                if (i == 0 || j == 0) {
+                    dp[i][j] = 0;
+                    continue;
+                }
+                int l = dp[i - 1][j], r = dp[i][j - 1];
+                dp[i][j] = grid[i - 1][j - 1];
 
-    public static void path(int[][] grid, boolean[][] visited, int m, int n, int x, int y, int sum, int ans) {
-        // System.out.println(x + " " + y);
-        if (sum > ans || x < 0 || y < 0 || x >= m || y >= n || visited[x][y])
-            return;
-        if (x == m - 1 && y == n - 1) {
-            ans = Math.min(ans, sum + grid[x][y]);
-            return;
+                if (i == 1)
+                    dp[i][j] += dp[i][j - 1];
+                if (j == 1)
+                    dp[i][j] += dp[i - 1][j];
+
+                if (i > 1 && j > 1)
+                    dp[i][j] += Math.min(dp[i - 1][j], dp[i][j - 1]);
+            }
         }
-        visited[x][y] = true;
-        path(grid, visited, m, n, x + 1, y, sum + grid[x][y], ans);
-        path(grid, visited, m, n, x, y + 1, sum + grid[x][y], ans);
-        path(grid, visited, m, n, x - 1, y, sum + grid[x][y], ans);
-        path(grid, visited, m, n, x, y - 1, sum + grid[x][y], ans);
-        visited[x][y] = false;
+        return dp[m][n];
     }
 
     public static void main(String[] args) throws Exception {
@@ -36,8 +36,7 @@ public class MinimumPathSum {
                 a[i][j] = scan.nextInt();
             }
         }
-        int ans = minPathSum(a);
-        System.out.println(ans);
+        System.out.println(minPathSum(a));
         scan.close();
     }
 }
