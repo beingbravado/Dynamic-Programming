ae3b230dd7514508a18766837dfb8bb049ffee23
beingbravado <sharmagyanesh199@gmail.com>
2021-05-24 13:41:24 +0530

    triangle

--- Triangle.java
+++ Triangle.java
@@ -1,11 +1,12 @@
-import java.util.*;
+import java.util.ArrayList;
+import java.util.Scanner;
 
 public class Triangle {
-    public static int minimumTotal(List<List<Integer>> triangle) {
-        List<List<Integer>> dp = new List<>();
+    public static int minimumTotal(ArrayList<ArrayList<Integer>> triangle) {
+        ArrayList<ArrayList<Integer>> dp = new ArrayList<>();
         dp.add(triangle.get(triangle.size() - 1));
         for (int i = 1; i < triangle.size(); i++) {
-            dp.add(new List<>());
+            dp.add(new ArrayList<>());
             for (int j = 0; j < triangle.size() - i; j++) {
                 dp.get(i).add(triangle.get(triangle.size() - i - 1).get(j)
                         + Math.min(dp.get(i - 1).get(j), dp.get(i - 1).get(j + 1)));
@@ -17,10 +18,10 @@ public class Triangle {
 
     public static void main(String[] args) throws Exception {
         Scanner scan = new Scanner(System.in);
-        List<List<Integer>> triangle = new List<>();
+        ArrayList<ArrayList<Integer>> triangle = new ArrayList<>();
         int n = scan.nextInt();
         for (int i = 0; i < n; i++) {
-            triangle.add(new List<>());
+            triangle.add(new ArrayList<>());
             for (int j = 0; j <= i; j++) {
                 triangle.get(i).add(scan.nextInt());
             }
