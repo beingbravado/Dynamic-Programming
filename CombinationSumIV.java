7537b4acba1b3287922bd0b0bad8eaae550b256c
beingbravado <sharmagyanesh199@gmail.com>
2021-05-25 13:43:02 +0530

    combination sum 4

---
+++ CombinationSumIV.java
@@ -0,0 +1,29 @@
+import java.util.Scanner;
+
+public class CombinationSumIV {
+    public static int combinationSum4(int[] nums, int target) {
+        int[] dp = new int[target + 1];
+        dp[0] = 1;
+        for (int j = 1; j <= target; j++) {
+            for (int i = 0; i < nums.length; i++) {
+                if (j >= nums[i]) {
+                    dp[j] += dp[j - nums[i]];
+                }
+                // System.out.print(dp[j] + " ");
+            }
+            // System.out.println();
+        }
+        return dp[target];
+    }
+
+    public static void main(String[] args) throws Exception {
+        Scanner scan = new Scanner(System.in);
+        int n = scan.nextInt();
+        int target = scan.nextInt();
+        int[] matrix = new int[n];
+        for (int j = 0; j < n; j++)
+            matrix[j] = scan.nextInt();
+        System.out.println(combinationSum4(matrix, target));
+        scan.close();
+    }
+}
